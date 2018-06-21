6bd744a8c943501bed3cd2f09e0f99d12531ee5f
vic6 <vicmatthews116@gmail.com>
2018-06-21 11:42:25 -0700

    Add ExpenseForm andd pass props

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ExpenseForm extends Component {
+  render() {
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,13 +4,22 @@ import EventForm from './EventForm';
 
 export default class Home extends Component {
   render() {
-    return  (
+    return (
       <Container>
-        <Header as='h1'>Welcome to Julius
+        <Header as="h1">
+          Welcome to Julius
           <Header.Subheader>Create a new event to begin</Header.Subheader>
         </Header>
-        <EventForm />
+        <EventForm
+          eventName={this.props.eventName}
+          eventNameChange={this.props.eventNameChange}
+          submitEvent={this.props.submitEvent}
+          addParticipant={this.props.addParticipant}
+          removeParticipant={this.props.removeParticipant}
+          handleChange={this.props.handleChange}
+          renderForms={this.props.renderForms}
+        />
       </Container>
-    )
+    );
   }
 }
